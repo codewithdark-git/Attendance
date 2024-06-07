fba88060fc94e19126c3b2834c2efb31584ebecd
Codewithdark <codewithdark90@gmail.com>
2024-06-07 21:17:12 +0500

    Create config.py

---
+++ Attendance_system/config.py
@@ -0,0 +1,14 @@
+import os
+from datetime import date, datetime
+
+# Paths
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FACE_DIR = os.path.join(BASE_DIR, 'data', 'faces')
+ATTENDANCE_DIR = os.path.join(BASE_DIR, 'data', 'Attendance')
+CASCADE_PATH = os.path.join(BASE_DIR, 'data', 'haarcascade_frontalface_default.xml')
+
+# Constants
+NIMGS = 5
+CURRENT_DATE = datetime.now().strftime("%d-%b-%Y")
+CURRENT_TIME = datetime.now().strftime("%H:%M:%S")
+CURRENT_MONTH = date.today().strftime("%B")
