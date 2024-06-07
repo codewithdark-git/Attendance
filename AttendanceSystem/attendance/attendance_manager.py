d82e28eefbcc7b5250972a0446702575f65117fc
Codewithdark <codewithdark90@gmail.com>
2024-06-07 23:36:00 +0500

    Update attendance_manager.py

--- AttendanceSystem/attendance/attendance_manager.py
+++ AttendanceSystem/attendance/attendance_manager.py
@@ -3,14 +3,15 @@ import csv
 import cv2
 from datetime import date
 from AttendanceSystem.attendance.attendance_file import get_attendance_file_path, create_attendance_csv, is_attendance_recorded, save_attendance
-from AttendanceSystem.face_recongnition.face_identification import extract_faces
+from AttendanceSystem.face_recognition.face_identification import extract_faces
 
 NIMGS = 5
 
 def add(new_user, program_name):
     new_user_name, new_user_id, program_name = new_user.split('_')
 
-    user_image_folder = os.path.join('face', f'{program_name}', f'{new_user_name}_{new_user_id}')
+    user_image_folder = os.path.join('face', f'{program_name}',
+                                     f'{new_user_name}_{new_user_id}')
     if not os.path.isdir(user_image_folder):
         os.makedirs(user_image_folder)
     i, j = 0, 0
@@ -20,7 +21,8 @@ def add(new_user, program_name):
         faces = extract_faces(frame)
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 20), 2)
-            cv2.putText(frame, f'Images Captured: {i}/{NIMGS}', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
+            cv2.putText(frame, f'Images Captured: {i}/{NIMGS}', (30, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
             if j % 5 == 0:
                 name = f'{new_user_name}_{new_user_id}_{i}.jpg'
                 cv2.imwrite(os.path.join(user_image_folder, name), frame[y:y + h, x:x + w])
