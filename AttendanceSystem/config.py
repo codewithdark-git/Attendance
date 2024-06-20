92ffd1838d5db8f2e28cf362415ac8fae301b203
Codewithdark <codewithdark90@gmail.com>
2024-06-20 20:05:27 +0500

    Update config.py

--- AttendanceSystem/config.py
+++ AttendanceSystem/config.py
@@ -1,8 +1,6 @@
-# config.py
-import os
 from datetime import datetime
 
-# Constants
-CURRENT_DATE_AND_TIME = datetime.now().strftime("%H-%M-%d-%b-%Y")
-CURRENT_DATE = datetime.now().strftime("%d-%b-%y")
 CURRENT_TIME = datetime.now().strftime("%H:%M:%S")
+CURRENT_DATE = datetime.now()  # Store as a datetime object
+CURRENT_MONTH = CURRENT_DATE.strftime("%B")
+DATABASE_URL = 'sqlite:///attendance.db'
