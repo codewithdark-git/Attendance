7befff24ac82edffa40b56c7f725aa6880f48c94
Codewithdark <codewithdark90@gmail.com>
2024-06-23 14:48:38 +0500

    Update face_utils.py

--- AttendanceSystem/face_recognition/face_utils.py
+++ AttendanceSystem/face_recognition/face_utils.py
@@ -1,6 +1,16 @@
 import cv2
 
 def extract_faces(img, face_detector):
+    """
+    Extracts faces from the input image using the provided face detector.
+
+    Args:
+        img: The input image from which faces will be extracted.
+        face_detector: The face detector object used to detect faces in the image.
+
+    Returns:
+        face_points: A list of rectangles representing the coordinates of the detected faces in the image.
+    """
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     face_points = face_detector.detectMultiScale(gray, 1.2, 5, minSize=(20, 20))
     return face_points
