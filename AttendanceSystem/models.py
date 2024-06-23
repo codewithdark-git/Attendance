03af1611c441ede600c333e2435a1fd8b8850bec
Codewithdark <codewithdark90@gmail.com>
2024-06-23 14:48:42 +0500

    Update models.py

--- AttendanceSystem/models.py
+++ AttendanceSystem/models.py
@@ -11,6 +11,17 @@ session = Session()
 Base = declarative_base()
 
 class User(Base):
+    """
+        User class represents the users table in the database.
+
+        Attributes:
+            id (int): Primary key of the user.
+            name (str): Name of the user.
+            user_id (str): Unique identifier of the user.
+            program_name (str): Name of the program associated with the user.
+            faces (relationship): Relationship to the Face class.
+            attendance (relationship): Relationship to the Attendance class.
+        """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
@@ -20,6 +31,15 @@ class User(Base):
     attendance = relationship('Attendance', back_populates='user')
 
 class Face(Base):
+    """
+        Face class represents the faces table in the database.
+
+        Attributes:
+            id (int): Primary key of the face.
+            user_id (int): Foreign key referencing the user ID.
+            image_data (str): Base64 encoded image data of the face.
+            user (relationship): Relationship to the User class.
+        """
     __tablename__ = 'faces'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
@@ -27,15 +47,36 @@ class Face(Base):
     user = relationship('User', back_populates='faces')
 
 class Attendance(Base):
+    """
+        Attendance class represents the attendance table in the database.
+
+        Attributes:
+            id (int): Primary key of the attendance record.
+            user_id (int): Foreign key referencing the user ID.
+            check_in_time (DateTime): Time when the user checked in.
+            check_out_time (DateTime): Time when the user checked out.
+            user (relationship): Relationship to the User class.
+        """
     __tablename__ = 'attendance'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     subject = Column(String, nullable=False)
     date = Column(DateTime, default=datetime.now, nullable=False)
     user = relationship('User', back_populates='attendance')
-    __table_args__ = (UniqueConstraint('user_id', 'subject', 'date', name='_attendance_uc'),)
+    __table_args__ = (UniqueConstraint('user_id', 'subject', 'date', name='_user_subject_date_uc'),)
 
 def create_tables():
+    """
+    Creates all the tables defined in the metadata of the `Base` class using the `engine` provided.
+
+    This function is responsible for creating all the tables in the database that are defined in the metadata of the `Base` class. It uses the `create_all` method of the `Base.metadata` object to create all the tables.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     Base.metadata.create_all(engine)
 
 create_tables()
